abd1a6e276cbfc74b685a06af0c1847af691838e
YoungsooJang <ylouang@kaist.ac.kr>
2017-01-06 05:14:17 +0000

    Server - convert amr to wav

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var dirname = "/home/youngsoo/SherlockHums/server/Recorded";
-var recordedFileName = "recordedFile.amr";
+var recordedFileAMR = "recordedFile.amr";
+var recordedFileWAV = "recordedFile.wav"; 
 var chunks = [];
 
 module.exports = function(app, Music){
@@ -11,11 +12,18 @@ module.exports = function(app, Music){
         console.log("Get page!");
     });
 
-    // GET RECORDED FILE
-    app.get('/recorded', function(req, res){
-        res.sendFile(dirname + '/' + recordedFileName);
-        console.log("Get recorded file!");
+    // GET RECORDED FILE (.amr)
+    app.get('/recorded.amr', function(req, res){
+        res.sendFile(dirname + '/' + recordedFileAMR);
+        console.log("Get recorded file(.amr)!");
     });
+
+    // GET RECORDED FILE (.wav)
+    app.get('/recorded.wav', function(req, res){
+        res.sendFile(dirname + '/' + recordedFileWAV);
+        console.log("Get recorded file(.wav)!");
+    });
+
 
     // UPLOAD RECORDED FILE
     app.post('/recorded', function(req, res){
@@ -26,7 +34,7 @@ module.exports = function(app, Music){
 
         req.on('end', function(){
             var data = Buffer.concat(chunks);
-            fs.writeFile(dirname + "/" + recordedFileName, data, 'binary', function(err){
+            fs.writeFile(dirname + "/" + recordedFileAMR, data, 'binary', function(err){
                 if (err) {
                     console.log("Can't upload recorded file! " + err);
                 } else {
@@ -36,7 +44,38 @@ module.exports = function(app, Music){
             });
             res.send("Upload of the recorded file is successful!");
             res.end();
+        })
+        runScript('./amrToWav.js', function(err){
+            if (err) throw err;
+            console.log('Converting amr to wav is complete!');
         });
     });
 
 }
+
+var childProcess = require('child_process');
+
+function runScript(scriptPath, callback) {
+
+    // keep track of whether callback has been invoked to prevent multiple invocations
+    var invoked = false;
+
+    var process = childProcess.fork(scriptPath);
+
+    // listen for errors as they may prevent the exit event from firing
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    // execute the callback once the process has finished running
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+
+}
+
