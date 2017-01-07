e0d1f954f2dd1f87b75f91db469e414cc35874db
choch-o <hscho1226@gmail.com>
2017-01-07 20:57:20 +0900

    Fixed orientation = landscape

--- android/MyApplication/app/src/main/java/com/example/q/project3/ReadyActivity.java
+++ android/MyApplication/app/src/main/java/com/example/q/project3/ReadyActivity.java
@@ -1,10 +1,28 @@
 package com.example.q.project3;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by q on 2017-01-07.
  */
 
 public class ReadyActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ready);
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ReadyActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 }
