d3f74da3cd47a7da1228d9158322db7b9c1d3250
choch-o <hscho1226@gmail.com>
2017-01-07 22:52:19 +0900

    Set orientation 'portrait'

--- android/MyApplication/app/src/main/java/com/example/q/project3/PlayingActivity.java
+++ android/MyApplication/app/src/main/java/com/example/q/project3/PlayingActivity.java
@@ -2,14 +2,19 @@ package com.example.q.project3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by q on 2017-01-07.
  */
 
 public class PlayingActivity extends Activity {
+    int currentRecorder = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +22,13 @@ public class PlayingActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_playing);
+        startRound();
+    }
+
+    private void startRound() {
+        int id = getResources().getIdentifier("player" + currentRecorder, "id", getPackageName());
+        LinearLayout recorder = (LinearLayout) findViewById(id);
+        recorder.setBackgroundColor(getResources().getColor(R.color.colorRecorder));
+
     }
 }
