70dc31c8fdcdb2378fe36bffc800ebe0daf21d1e
YoungsooJang <ylouang@kaist.ac.kr>
2017-01-07 21:31:37 +0900

    no Action Bar

--- android/MyApplication/app/src/main/java/com/example/q/project3/ReadyActivity.java
+++ android/MyApplication/app/src/main/java/com/example/q/project3/ReadyActivity.java
@@ -1,16 +1,13 @@
 package com.example.q.project3;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-/**
- * Created by q on 2017-01-07.
- */
+public class ReadyActivity extends AppCompatActivity {
 
-public class ReadyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,4 +22,5 @@ public class ReadyActivity extends Activity {
             }
         });
     }
+
 }
