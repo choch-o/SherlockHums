375223e6d4cda39a01103f2e05af2181c12db4da
YoungsooJang <ylouang@kaist.ac.kr>
2017-01-08 23:22:39 +0900

    minor changes about permission

--- android/MyApplication/app/src/main/java/com/example/q/project3/ReadyActivity.java
+++ android/MyApplication/app/src/main/java/com/example/q/project3/ReadyActivity.java
@@ -1,6 +1,8 @@
 package com.example.q.project3;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,6 +16,10 @@ import java.util.ArrayList;
 
 public class ReadyActivity extends AppCompatActivity {
 
+    private static final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 2;
+    private static final int PERMISSIONS_REQUEST_INTERNET = 3;
+
     ArrayList<String[]> ranks = new ArrayList<>();
 
     @Override
@@ -23,6 +29,16 @@ public class ReadyActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_ready);
+
+        // Check the SDK version and whether the permission is already granted or not.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            if (this.checkSelfPermission(android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
+                requestPermissions(new String[]{android.Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
+            if (this.checkSelfPermission(android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED)
+                requestPermissions(new String[]{android.Manifest.permission.INTERNET}, PERMISSIONS_REQUEST_INTERNET);
+        }
 
         Button startButton = (Button) findViewById(R.id.start_button);
         startButton.setOnClickListener(new View.OnClickListener() {
--- android/MyApplication/app/src/main/java/com/example/q/project3/SplashScreen.java
+++ android/MyApplication/app/src/main/java/com/example/q/project3/SplashScreen.java
@@ -1,9 +1,6 @@
 package com.example.q.project3;
 
-import android.*;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
@@ -13,9 +10,6 @@ import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 
 public class SplashScreen extends AppCompatActivity {
-    private static final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
-    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 2;
-    private static final int PERMISSIONS_REQUEST_INTERNET = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +20,6 @@ public class SplashScreen extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.splash);
-
-        // Check the SDK version and whether the permission is already granted or not.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (this.checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-                requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
-            if (this.checkSelfPermission(android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED)
-                requestPermissions(new String[]{android.Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
-            if (this.checkSelfPermission(android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED)
-                requestPermissions(new String[]{android.Manifest.permission.INTERNET}, PERMISSIONS_REQUEST_INTERNET);
-        }
 
         Thread timerThread = new Thread() {
             public void run(){
